d4c7687bdeecef3f53375687a73995bb4bfc1713
RedMage <xaioxaiofan@gmail.com>
2023-12-05 17:55:45 +1300

    Day 5

---
+++ AOC2023/Day5/CSharp/AOC2023-Day5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AOC2023_Day5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+const string IntroFile = @"D:\AdventOfCodeAllYears\AOC2023\Day5\Inputs\intro.txt";
+const string ActualFile = @"D:\AdventOfCodeAllYears\AOC2023\Day5\Inputs\actual.txt";
