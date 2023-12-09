773cdc9f2973b4416cc4e3bdaa22bd3174fbb86e
RedMage <xaioxaiofan@gmail.com>
2023-12-09 15:21:00 +1300

    Day 9 Created

---
+++ AOC2023/Day9/CSharp/AOC2023-Day9/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AOC2023_Day9;
+
+class Program
+{
+    const string IntroFile = @"D:\AdventOfCodeAllYears\AOC2023\Day9\Inputs\intro.txt";
+    const string ActualFile = @"D:\AdventOfCodeAllYears\AOC2023\Day9\Inputs\actual.txt";
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Intro: {PartOne(IntroFile)}");
+        Console.WriteLine("Part One: {}");
+        Console.WriteLine("Part Two: {}");
+    }
+
+    static long PartOne(string file) {
+        var lines = File.ReadAllLines(file);
+
+        return 0L;
+    }
+    
+}
+
---
+++ AOC2023/Day9/FSharp/AOC2023-Day9/Program.fs
@@ -0,0 +1,2 @@
+let IntroFile = @"D:\AdventOfCodeAllYears\AOC2023\Day9\Inputs\intro.txt";
+let ActualFile = @"D:\AdventOfCodeAllYears\AOC2023\Day9\Inputs\actual.txt";
