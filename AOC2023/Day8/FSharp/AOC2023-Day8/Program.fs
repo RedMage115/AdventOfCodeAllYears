30dcc182efee8a31dde02d9b30d3517aba0342e2
RedMage <xaioxaiofan@gmail.com>
2023-12-09 15:10:19 +1300

    2023 Day 8 part 2 F#

--- AOC2023/Day8/FSharp/AOC2023-Day8/Program.fs
+++ AOC2023/Day8/FSharp/AOC2023-Day8/Program.fs
@@ -1,3 +1,5 @@
+open System
+open System.Collections.Generic
 open System.IO
 
 let IntroFile = @"D:\AdventOfCodeAllYears\AOC2023\Day8\Inputs\intro.txt";
@@ -7,6 +9,7 @@ type Node = {
     root: string
     left: string
     right: string
+    mutable steps: Int64
 }
 
 type LineType =
@@ -35,7 +38,7 @@ let ParseNodeFromString(text:string) =
     let directions = text.Split '=' |> Seq.tail |> Seq.map(fun dir -> dir.Trim().Trim('(').Trim(')')) |> Seq.toList
     let left = directions.Head.Split ", " |> Seq.head
     let right = directions.Head.Split ", " |> Seq.tail |> Seq.head
-    {root = root; left = left; right = right; }
+    {root = root; left = left; right = right; steps = 0; }
 
 
 let PartOne(file:string) =
@@ -53,9 +56,41 @@ let PartOne(file:string) =
     steps
 
 
+let GCF(x,y) =
+    let mutable a:Int64 = x
+    let mutable b:Int64 = y
+    while not(b = 0) do
+        let temp = b
+        b <- a % b
+        a <- temp
+    a
+
+
+let LCM x y =
+    (x / GCF(x,y)) * y
+    
+
+let PartTwo(file:string) =
+    let lines = LineList file
+    let dirLine = ParseLines(lines, LineType.DirLine).Head |> Seq.toList
+    let nodeLines = ParseLines(lines, LineType.NodeLine) |> List.map(ParseNodeFromString)
+    let mutable currentNodes = nodeLines |> List.filter(fun n -> n.root |> Seq.last = 'A')
+    for x in 0..currentNodes.Length - 1 do
+        let mutable cNode = currentNodes[x]
+        while not (cNode.root |> Seq.last = 'Z') do
+            for dir in dirLine do
+                match dir with
+                | 'L' -> cNode <- nodeLines |> List.find(fun n -> n.root = cNode.left)
+                | 'R' -> cNode <- nodeLines |> List.find(fun n -> n.root = cNode.right)
+                currentNodes[x].steps <- currentNodes[x].steps + 1L
+    currentNodes |> List.map(_.steps) |> List.reduce(LCM) 
+
+
+
 
 [<EntryPoint>]
 let main(args) =
     printfn $"Intro: %d{PartOne(IntroFile)}" 
     printfn $"Part One: %d{PartOne(ActualFile)}" 
+    printfn $"Part Two: %d{PartTwo(ActualFile)}" 
     0
